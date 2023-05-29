c321f47f92b4007e5144c6cca6eba53f5f69ea56
chaeunho1227 <chaeunho2002@gmail.com>
2023-05-29 23:07:59 +0900

    7주차 session

--- main/views.py
+++ main/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Blog
+from .models import Blog, Comment, Tag
 from django.utils import timezone
 
 def detail(request, id):
     blog = get_object_or_404(Blog, pk = id)
-    return render(request, 'main/detail.html',{'blog':blog})
+    if request.method == 'GET':
+        comments = Comment.objects.filter(blog=blog)
+        return render(request, 'main/detail.html',{
+            'blog':blog,
+            'comments':comments,
+        })
+    elif request.method == 'POST':
+        new_comment = Comment()
+        new_comment.blog = blog
+        new_comment.writer = request.user
+        new_comment.content = request.POST['content']
+        new_comment.pub_date = timezone.now()
+        new_comment.save()
+        return redirect('main:detail',id)
 
 def mainpage(request):
     blogs = Blog.objects.all()
@@ -14,16 +27,30 @@ def secondpage(request):
     return render(request, 'main/secondpage.html')
 
 def create(request):
-    new_blog = Blog()
-    new_blog.title = request.POST['title']
-    new_blog.writer = request.POST['writer']
-    new_blog.pub_date = timezone.now()
-    new_blog.body = request.POST['body']
-    new_blog.image = request.FILES.get('image')
+    if request.user.is_authenticated:
+        new_blog = Blog()
+        new_blog.title = request.POST['title']
+        new_blog.writer = request.user
+        new_blog.pub_date = timezone.now()
+        new_blog.body = request.POST['body']
+        new_blog.image = request.FILES.get('image')
 
-    new_blog.save()
-    
-    return redirect('main:detail', new_blog.id)
+        new_blog.save()
+
+        words = new_blog.body.split(' ')
+
+        tag_list = []
+        for w in words:
+            if len(w) > 0 and w[0] == '#':
+                tag_list.append(w[1:])
+
+        for t in tag_list:
+            tag, boolean = Tag.objects.get_or_create(name=t)
+            new_blog.tags.add(tag.id)
+        
+        return redirect('main:detail', new_blog.id)
+    else:
+        return redirect('accounts:login')
 
 def new(request):
     return render(request, 'main/new.html')
@@ -48,3 +75,17 @@ def delete(request, id):
     delete_blog = Blog.objects.get(id=id)
     delete_blog.delete()
     return redirect('main:mainpage')
+
+def tag_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'main/tag_list.html',{
+        'tags':tags,
+    })
+
+def tag_blogs(request, tag_id):
+    tag = get_object_or_404(Tag, id=tag_id)
+    blogs = tag.blogs.all()
+    return render(request,'main/tag_blogs.html',{
+        'tag':tag,
+        'blogs':blogs,
+    })
